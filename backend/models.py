2dd00232d21210542dc48eb79d19cf17ffa8f8d9
William Saleh <99556033+WillSaleh@users.noreply.github.com>
2026-04-19 00:51:25 -0700

    add User and ReadingLog models for database

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+# 2 models for the database: User and Reading log 
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from datetime import date
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str
+    hashed_password: str
+
+class ReadingLog(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    passage: str
+    date: date
