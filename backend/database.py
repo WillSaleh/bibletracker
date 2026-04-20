f5c3fa651cd7cd23f20229167050383e46e78314
William Saleh <99556033+WillSaleh@users.noreply.github.com>
2026-04-20 16:41:26 -0700

    initialize database connection and session management

---
+++ backend/database.py
@@ -0,0 +1,13 @@
+# Sets up connection between python and database
+
+from sqlmodel import SQLModel, create_engine, Session
+
+DATABASE_URL = "sqlite:///./reading_log.db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db():
+    SQLModel.meteadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
